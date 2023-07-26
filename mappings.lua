30294c1987ddf38cf2836c2259cc53145105bb16
jw26 <jeffwise26@gmail.com>
2023-07-26 17:36:36 -0600

    terminal navigation, github theme, show hidden files by default

--- mappings.lua
+++ mappings.lua
@@ -24,5 +24,7 @@ return {
   t = {
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
+    -- leave terminal with escape, navigate up one
+    ["<esc>"] = "<Cmd>wincmd k<CR>"
   },
 }
